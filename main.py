b2a3efca82a66d8ffd3db9ab9226c156a436fab6
Ankit Khadka <khadka.ankit2004@gmail.com>
2025-07-17 08:06:34 +0545

    Making the polling and add_handler and completing the bot

--- main.py
+++ main.py
@@ -40,4 +40,46 @@ def handle_response(text: str) -> str:
     return "Ae bhai i am still under development , only use 'hi','hello','thankyou','how are you'. BUJEU? / TYPE nai"
     
     
+async def handle_message(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(f'User ({update.message.chat.id}) in {message_type}: {text}')
+    
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+            
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+        
+    print('Bot:',response)
+    await update.message.reply_text(response)
+    
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+    
+if __name__ == '__main__':
+    print('STARTING BOT.....')
+    app = Application.builder().token(TOKEN).build()
+    
+    # COMMANDS
+    app.add_handler(CommandHandler('start',start_command))
+    app.add_handler(CommandHandler('help',help_command))
+    app.add_handler(CommandHandler('custom',custom_command))
+    
+    # MESSAGES
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    # ERRORS
+    app.add_error_handler(error)
+    
+    # POLLING THE BOT
+    print('POLLING....')
+    app.run_polling(poll_interval=2)
+    
+    
     
